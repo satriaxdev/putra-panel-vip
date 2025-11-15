e844da79ae36bcc18ca098859586adc986672679
satriaxdev <satriadeveloperz@gmail.com>
2025-11-15 18:47:53 +0700

    Add A PANEL V993/js/main.js

---
+++ A PANEL V993/js/main.js
@@ -0,0 +1,17 @@
+// Notifications
+function showNotif(text){ /* ... isi sama seperti di index.html ... */ }
+
+// ROLE card helpers
+function setROLECard(role, username){ /* ... */ }
+(function restoreROLEOnLoad(){ /* ... */ })();
+
+// Toggle checkbox notifications
+document.addEventListener('change', function(e){ /* ... */ });
+
+// Particle background
+(function particlesMain(){ /* ... */ })();
+(function particlesLogin(){ /* ... */ })();
+
+// misc helpers
+function closePanel(){ /* ... */ }
+document.addEventListener('DOMContentLoaded', ()=> { renderUploadList(); });
